5d6cb7d32cc31babf7bf759de10f5b4ae736ba4e
adrianoaurelio <adrianoaurelio.souza@gmail.com>
2026-09-13 21:06:49 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,12 +1,16 @@
 from src.main import *
 from unittest.mock import patch
 
+import pytest
 
+
+@pytest.mark.asyncio
 async def test_root():
     result = await root()
     assert result == {"message": "Hello World"}
 
 
+@pytest.mark.asyncio
 async def test_funcaoteste():
     with patch("random.randint", return_value=12345):
         result = await funcaoteste()
@@ -17,6 +21,7 @@ async def test_funcaoteste():
     }
 
 
+@pytest.mark.asyncio
 async def test_create_estudante():
     estudante_teste = Estudante(
         name="Fulano",
@@ -29,21 +34,25 @@ async def test_create_estudante():
     assert estudante_teste == result
 
 
+@pytest.mark.asyncio
 async def test_update_estudante_negativo():
     result = await update_estudante(-5)
     assert not result
 
 
+@pytest.mark.asyncio
 async def test_update_estudante_positivo():
     result = await update_estudante(10)
     assert result
 
 
+@pytest.mark.asyncio
 async def test_delete_estudante_negativo():
     result = await delete_estudante(-5)
     assert not result
 
 
+@pytest.mark.asyncio
 async def test_delete_estudante_positivo():
     result = await delete_estudante(5)
     assert result
